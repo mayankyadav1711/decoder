7c5a918eed119765726650a1363a57309345a8a6
mayankyadav1711 <mykyadav2003@gmail.com>
2024-06-20 10:59:49 +0530

    link test done

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -2,13 +2,14 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import RepositoriesListItem from "./RepositoriesListItem";
 
-
 function renderComponent() {
   const repository = {
     full_name: "facebook/react",
     language: "Javascript",
     description: "good project",
-    owner: "myk",
+    owner: {
+        login:'facebook'
+    },
     name: "react",
     html_url: "https://github.com/facebook/react",
   };
@@ -17,20 +18,29 @@ function renderComponent() {
       <RepositoriesListItem repository={repository} />
     </MemoryRouter>
   );
-  return {repository};
+  return { repository };
 }
 test("Shows a link to the github homepage for repository", async () => {
-  const {repository} =  renderComponent();
-  await screen.findByRole('img', {name : 'Javascript'});
-  const link = screen.getByRole('link', {
+  const { repository } = renderComponent();
+  await screen.findByRole("img", { name: "Javascript" });
+  const link = screen.getByRole("link", {
     name: /github repository/i,
-  })
-  expect(link).toHaveAttribute('href', repository.html_url)
-
+  });
+  expect(link).toHaveAttribute("href", repository.html_url);
 });
 
-test('shows a fileicon with an appropriate icon', async()=>{
-renderComponent();
-const icon = await screen.findByRole('img', {name: 'Javascript'})
-expect(icon).toHaveClass('js-icon')
+test("shows a fileicon with an appropriate icon", async () => {
+  renderComponent();
+  const icon = await screen.findByRole("img", { name: "Javascript" });
+  expect(icon).toHaveClass("js-icon");
 });
+
+
+test('show a link to the code editor page', async()=>{
+    const {repository}= renderComponent();
+    await screen.findByRole('img',{name: "Javascript"})
+    const link = await screen.findByRole('link',{
+        name : new RegExp(repository.owner.login)
+    })
+    expect(link).toHaveAttribute('href', `/repositories/${repository.full_name}`)
+})
