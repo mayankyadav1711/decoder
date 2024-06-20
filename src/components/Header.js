76ab449dd16a66b13f929f966ac39f34af64214f
mayankyadav1711 <mykyadav2003@gmail.com>
2024-06-20 15:38:16 +0530

    header update, hero fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import AuthButtons from './auth/AuthButtons';
 import SearchBar from './search/SearchBar';
-
+import logo from './electron-1.svg'
 function Header() {
   return (
     <header
       aria-label="Site Header"
-      className=" top-0 left-0 right-0 z-50 bg-neutral-900 backdrop-blur-xl"
+      className="top-0 left-0 right-0 z-50 bg-neutral-900 backdrop-blur-xl"
     >
-      <div className="mx-auto flex h-16 max-w-screen-xl items-center justify-between gap-8 px-4 sm:px-6 lg:px-8">
+       <div className="mx-auto flex h-16 max-w-screen-xl items-center justify-between px-4 sm:px-6 lg:px-8">
+        {/* Logo image */}
+        <img src={logo} alt="Logo" className="h-12 w-auto mr-[-29rem]" />
+
+        {/* Site title */}
         <Link
-          className="text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-purple-300"
+          className="coolfont text-3xl font-extrabold text-gradient bg-gradient-to-r bg-clip-text from-blue-300 to-purple-300 mt-1"
           to="/"
         >
-          CodeSplain
+          DeCoder
         </Link>
 
         <div className="flex items-center gap-6">
@@ -31,7 +35,6 @@ function Header() {
           </div>
         </div>
       </div>
-
     </header>
   );
 }
