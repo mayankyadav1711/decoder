a9cdccf4cc6879f455f34edd969c2d7eaa1fb230
mayankyadav1711 <mykyadav2003@gmail.com>
2024-06-20 10:33:23 +0530

    List Item Testing done

---
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import RepositoriesListItem from "./RepositoriesListItem";
+
+function renderComponent() {
+  const repository = {
+    full_name: "facebook/react",
+    language: "python",
+    description: "good project",
+    owner: "myk",
+    name: "react",
+    html_url: "https://github.com/facebook/react",
+  };
+  render(
+    <MemoryRouter>
+      <RepositoriesListItem repository={repository} />
+    </MemoryRouter>
+  );
+}
+test("Shows a link to the github homepage for repository", async () => {
+  renderComponent();
+  await screen.findByRole('img', {name : 'python'});
+
+});
+
+const pause = () =>{
+    return new Promise(resolve=>{
+        setTimeout(()=>{
+            resolve();
+        },100)
+    })
+}
