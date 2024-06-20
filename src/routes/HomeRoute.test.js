699763c32eee0cc100963ff0d0e908fe61e059c2
mayankyadav1711 <mykyadav2003@gmail.com>
2024-06-20 11:53:39 +0530

    making reusable server function - better approach

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -1,40 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import { setupServer } from 'msw/node';
-import { rest } from 'msw';
-import { MemoryRouter } from 'react-router';
-import HomeRoute from './HomeRoute';
+import { render, screen } from "@testing-library/react";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+import { MemoryRouter } from "react-router";
+import HomeRoute from "./HomeRoute";
+import { createServer } from "../test/server";
 
-const handlers = [
-  rest.get('/api/repositories', (req, res, ctx) => {
-    const langauge = req.url.searchParams.get('q').split('language:')[1];
-    console.log(langauge);
+createServer([
+  {
+    path: "/api/repositories",
+    res: (req) => {
+        const langauge = req.url.searchParams.get("q").split("language:")[1];
 
-    return res(
-      ctx.json({
+      return {
         items: [
-          { id: 1, full_name: `${langauge}_one` },
-          { id: 2, full_name: `${langauge}_two` },
-        ],
-      })
-    );
-  }),
-];
+            { id: 1, full_name: `${langauge}_one` },
+            { id: 2, full_name: `${langauge}_two` },
+          ],
+      };
+    },
+  },
+]);
 
-const server = setupServer(...handlers);
 
-beforeAll(() => {
-  server.listen();
-});
 
-afterEach(() => {
-  server.resetHandlers();
-});
 
-afterAll(() => {
-  server.close();
-});
-
-test('renders two repo(links) for each language', async () => {
+test("renders two repo(links) for each language", async () => {
   render(
     <MemoryRouter>
       <HomeRoute />
@@ -43,24 +33,24 @@ test('renders two repo(links) for each language', async () => {
 
   //loop over each language
   const languages = [
-    'javascript',
-    'typescript',
-    'rust',
-    'go',
-    'python',
-    'java',
+    "javascript",
+    "typescript",
+    "rust",
+    "go",
+    "python",
+    "java",
   ];
   //for each langauge, make sure we see two links
   //assert that the links should have full_name
 
   for (let langauge of languages) {
-    const links = await screen.findAllByRole('link', {
+    const links = await screen.findAllByRole("link", {
       name: new RegExp(`${langauge}_`),
     });
     expect(links).toHaveLength(2);
-    expect(links[0]).toHaveTextContent(`${langauge}_one`)
-    expect(links[1]).toHaveTextContent(`${langauge}_two`)
-    expect(links[0]).toHaveAttribute('href',`/repositories/${langauge}_one`)
-    expect(links[1]).toHaveAttribute('href',`/repositories/${langauge}_two`)
+    expect(links[0]).toHaveTextContent(`${langauge}_one`);
+    expect(links[1]).toHaveTextContent(`${langauge}_two`);
+    expect(links[0]).toHaveAttribute("href", `/repositories/${langauge}_one`);
+    expect(links[1]).toHaveAttribute("href", `/repositories/${langauge}_two`);
   }
 });
